b7c04e91ec3399eab370a3c4b2d276b6a0d569ac
joaohdallago <joaohdallago@gmail.com>
2022-03-30 16:14:24 -0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <h1>app</h1>
+    )
+}
+
+const root = document.querySelector('.root');
+ReactDOM.render(<App />, root)
